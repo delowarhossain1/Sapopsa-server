a5d5b424d74af90164e99af0bac14839822ce5f2
Developer Delowar <daloar2400@gmail.com>
2023-04-08 11:37:26 -0700

    add new product updated

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const app = express();
 
 // Middlewares
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit:50000}));
 app.use(cors());
